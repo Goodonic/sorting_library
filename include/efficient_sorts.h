b3a93c7fd1e5a8e48bbfd683444256849a80bd94
Goodonic <orexovheketa@gmail.com>
2026-03-03 17:21:49 +0700

    add quicksort

--- include/efficient_sorts.h
+++ include/efficient_sorts.h
@@ -1,9 +1,7 @@
 #ifndef EFFICIENT_SORTS_H
 #define EFFICIENT_SORTS_H
 
-void quick_sort(void *arr, int n, size_t elem_size,
-                int (*cmp)(const void*, const void*),
-                long *comparisons, long *swaps);
+void quick_sort(int *s_arr,  int first,  int last);
 
 void merge_sort(void *arr, int n, size_t elem_size,
                 int (*cmp)(const void*, const void*),
@@ -20,4 +18,11 @@ void tim_sort(void *arr, int n, size_t elem_size,
 void intro_sort(void *arr, int n, size_t elem_size,
                 int (*cmp)(const void*, const void*),
                 long *comparisons, long *swaps);
+
+static void swap(int *a, int *b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
 #endif //EFFICIENT_SORTS_H
--- src/algorithms/efficient/quicksort.c
+++ src/algorithms/efficient/quicksort.c
@@ -1,3 +1,26 @@
-//
-// Created by orexo on 03.03.2026.
-//
+#include "efficient_sorts.h"
+
+int n, a[n];
+void quick_sort(int *s_arr, const int first, const int last)
+{
+  if (first < last)
+  {
+    int left = first, right = last;
+    const int middle = s_arr[(left + right) / 2];
+    do
+    {
+      while (s_arr[left] < middle) left++;
+      while (s_arr[right] > middle) right--;
+      if (left <= right)
+      {
+        int tmp = s_arr[left];
+        s_arr[left] = s_arr[right];
+        s_arr[right] = tmp;
+        left++;
+        right--;
+      }
+    } while (left <= right);
+    quick_sort(s_arr, first, right);
+    quick_sort(s_arr, left, last);
+  }
+}
